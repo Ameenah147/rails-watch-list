de016a671939dee5719dde8d14a857fc10b7d7d7
Ameenah Rostum <ameenah141991@gmail.com>
2022-10-06 14:56:37 +0400

    validation

--- db/migrate/20221006091050_create_bookmarks.rb
+++ db/migrate/20221006091050_create_bookmarks.rb
@@ -2,8 +2,8 @@ class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
       t.text :comment
-      t.integer :movie_id
-      t.integer :list_id
+      t.references :movie, foreign_key: true, null: false
+      t.references :list, foreign_key: true, null: false
 
       t.timestamps
     end
